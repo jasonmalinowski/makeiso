c12e1392bb08fb9d10c4c9d3f4660a08cb9bc4f8
Jason Malinowski <jason@jason-m.com>
2016-01-31 23:38:53 -0800

    Initial version
    This has exactly one feature: when given a drive letter and .iso, it
    copies the contents over. That's it.

---
+++ MakeISO/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using Microsoft.Win32.SafeHandles;
+
+namespace MakeISO
+{
+    public static class Program
+    {
+        [DllImport("kernel32.dll", EntryPoint = "CreateFileW", CharSet = CharSet.Unicode, SetLastError = true )]
+        private static extern SafeFileHandle CreateFile(
+            [MarshalAs(UnmanagedType.LPTStr)] string lpFileName,
+            uint dwDesiredAccess,
+            uint dwShareMode,
+            IntPtr lpSecurityAttributes,
+            uint dwCreationDisposition,
+            uint dwFlagsAndAttributes,
+            uint hTemplateFile);
+
+        private const uint GENERIC_READ = 0x80000000;
+        private const uint FILE_SHARE_READ = 0x00000001;
+        private const uint OPEN_EXISTING = 0x00000003;
+        private const uint FILE_FLAG_SEQUENTIAL_SCAN = 0x08000000;
+
+        public static int Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("usage: makeiso [drive] [file]");
+                return 1;
+            }
+
+            var drive = new DriveInfo(args[0]);
+
+            if (drive.DriveType != DriveType.CDRom)
+            {
+                Console.Error.WriteLine("Invalid drive letter.");
+                return 2;
+            }
+
+            try
+            {
+                using (var inputFileHandle = CreateFile(@"\\.\" + drive.ToString().TrimEnd('\\'), GENERIC_READ, FILE_SHARE_READ, IntPtr.Zero, OPEN_EXISTING, FILE_FLAG_SEQUENTIAL_SCAN, 0))
+                {
+                    if (inputFileHandle.IsInvalid)
+                    {
+                        Marshal.ThrowExceptionForHR(Marshal.GetHRForLastWin32Error());
+                    }
+
+                    using (var inputStream = new FileStream(inputFileHandle, FileAccess.Read))
+                    {
+                        using (var outputStream = new FileStream(args[1], FileMode.CreateNew))
+                        {
+                            inputStream.CopyTo(outputStream);
+                        }
+                    }
+                }
+
+                return 0;
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine(e.Message);
+                return 1;
+            }
+        }
+    }
+}
---
+++ MakeISO/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("MakeISO")]
+[assembly: AssemblyProduct("MakeISO")]
+[assembly: AssemblyCopyright("Copyright © 2016 Jason Malinowski")]
+
+[assembly: AssemblyVersion("0.1.0")]
+[assembly: AssemblyFileVersion("0.1.0")]
